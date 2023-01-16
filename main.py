e0e78dcd42e83ae6cde2ca44e5b2c36dffaac8dd
Cameron Fung <Cameronfuung@gmail.com>
2023-01-15 17:43:48 -0800

    chagned to pg for pygame

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame as pg
 
 # Initialize pygame library
 pygame.init()
@@ -7,16 +7,18 @@ pygame.init()
 window_size = (width, height) = (500, 500)
 
 # Create the window
-screen = pygame.display.set_mode(window_size)
+screen = pg.display.set_mode(window_size)
 
 # Set the title of the window
-pygame.display.set_caption("My Snake Game")
+pg.display.set_caption("My Snake Game")
+
+
 
 # Run the game loop
 running = True
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
             running = False
 
 # Clean up and quit pygame
