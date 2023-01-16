945d230a484cca545f611164764ebf6d5918dd38
Cameron Fung <Cameronfuung@gmail.com>
2023-01-15 17:57:42 -0800

    trying to draw the body to the screen

--- main.py
+++ main.py
@@ -13,11 +13,20 @@ screen = pg.display.set_mode(window_size)
 pg.display.set_caption("My Snake Game")
 
 #setting up the snake
+
+snake_x = 250
+snake_y = 250
+
+snake_size = 25
+
 snake_body = []
 
-snake_body.append()
+snake_head = pg.Rect(snake_x, snake_y, snake_size, snake_size)
+print(snake_head)
 
 #adding the head to the body
+snake_body.append(snake_head)
+
 
 # Run the game loop
 running = True
@@ -25,6 +34,10 @@ while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+
+    for rect in snake_body:
+        pg.draw.rect(screen, (255, 255, 255), rect)
+
 
 # Clean up and quit pygame
 pg.quit()
