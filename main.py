e07334916819b966e402c99d6e5774313b4003e0
Cameron Fung <Cameronfuung@gmail.com>
2023-01-15 17:34:36 -0800

    setting up the window

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-print("hello world")
-print("seb made this commit")
+import pygame
+
+# Initialize pygame library
+pygame.init()
+
+# Set the size of the window
+window_size = (width, height) = (500, 500)
+
+# Create the window
+screen = pygame.display.set_mode(window_size)
+
+# Set the title of the window
+pygame.display.set_caption("My Snake Game")
+
+# Run the game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+# Clean up and quit pygame
+pygame.quit()
